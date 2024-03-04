2063b7236e3cdc7d5acc0dc08a60a8d71e9f4d4d
HenryChinask1 <152664387+HenryChinask1@users.noreply.github.com>
2024-03-03 23:09:47 -0500

    Add files via upload

---
+++ Bagels.py
@@ -0,0 +1,12 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESS = 10
+
+def getSecretNumber():
+    # Returns a string of random digits that is NUM_DIGITS long.
+    numbers = list(range(10))
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
