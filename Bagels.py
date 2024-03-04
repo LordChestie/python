de1b44753f2d75b303ffad06c9439fe68e8647ea
Lorde <162139437+LordChestie@users.noreply.github.com>
2024-03-04 10:24:29 -0500

    Update Bagels.py
    spermin

--- Bagels.py
+++ Bagels.py
@@ -3,6 +3,8 @@ import random
 NUM_DIGITS = 3
 MAX_GUESS = 10
 
+#you know i am eat that turkey
+
 def getSecretNumber():
     # Returns a string of random digits that is NUM_DIGITS long.
     numbers = list(range(10))
